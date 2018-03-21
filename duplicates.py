b1a9b8ef5d27b13e0da3c682d2dab0c3ab7be6c8
OnlineAlex <becasualua@gmail.com>
2018-03-21 13:59:49 +0200

    Переименованы переменные, duplicate_files = {}

--- duplicates.py
+++ duplicates.py
@@ -3,18 +3,18 @@ import os
 import collections
 
 
-def get_all_files(start_folder):
-    all_files = collections.defaultdict(list)
-    for root, dirs, name_files in os.walk(start_folder):
-        for name_file in name_files:
-            file_size = os.path.getsize(os.path.join(root, name_file))
-            all_files[(name_file, file_size)].append(root)
+def get_all_files(dir_path):
+    files_locations = collections.defaultdict(list)
+    for root, dirs, file_names in os.walk(dir_path):
+        for file_name in file_names:
+            file_size = os.path.getsize(os.path.join(root, file_name))
+            files_locations[(file_name, file_size)].append(root)
 
-    return all_files
+    return files_locations
 
 
 def get_duplicate_files(all_files):
-    duplicate_files = collections.defaultdict(list)
+    duplicate_files = {}
     for (file_name, file_size), file_root in all_files.items():
         if len(file_root) > 1:
             duplicate_files[file_name] = file_root
@@ -22,13 +22,13 @@ def get_duplicate_files(all_files):
     return duplicate_files
 
 
-def print_duplicates(files_data):
-    if files_data:
-        print('Количество дублируемых файлов — {}'.format(len(files_data)))
-        for file_name, file_roots in files_data.items():
+def print_duplicates(duplicate_files):
+    if duplicate_files:
+        print('Количество дублируемых файлов — {}'.format(len(duplicate_files)))
+        for file_name, file_paths in duplicate_files.items():
             print('Файл "{}" дублируется в каталогах: \n{}\n'.format(
                 file_name,
-                '\n'.join(file_roots)
+                '\n'.join(file_paths)
             ))
     else:
         print('Дубликаты не найдены!')
